1f44f6f294c0009c835f6ef82f8e3046a7f6c49e
qnp <francois.risoud@gmail.com>
2018-07-14 14:12:39 +0200

    landing page ready

--- src/entries/index.js
+++ src/entries/index.js
@@ -4,3 +4,12 @@ import 'reset-css/reset.css';
 
 // import styles
 import '@/styles/index.styl';
+
+let menuOpen = false;
+const navContent = document.querySelector('.nav-content');
+const burger = document.querySelector('.burger');
+burger.addEventListener('click', function(e) {
+  if (!menuOpen) navContent.classList.add('open');
+  else navContent.classList.remove('open');
+  menuOpen = !menuOpen;
+});
